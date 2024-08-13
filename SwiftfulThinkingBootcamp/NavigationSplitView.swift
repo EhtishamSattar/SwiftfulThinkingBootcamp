9d19aca8abf9025b064445b1b0a912c9da53aec2
EhtishamSattar <ehtishamsattar888@gmail.com>
2024-08-13 19:54:05 +0500

    NavigationSplitView

---
+++ SwiftfulThinkingBootcamp/NavigationSplitView.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct NavigationSplitView2: View  {
+    @State private var visibility: NavigationSplitViewVisibility = .all
+    @State private var selectedCategory: FoodCategory? = nil
+    @State private var selectedFruit : Fruit? = nil
+
+    var body: some View {
+        NavigationSplitView(columnVisibility: $visibility) {
+            List {
+                ForEach(FoodCategory.allCases, id: \.rawValue) { category in
+                    Button(category.rawValue.capitalized) {
+                        selectedCategory = category
+                    }
+                }
+            }
+            .navigationTitle("Categories")
+        } content: {
+            if let selectedCategory {
+                Group {
+                    switch selectedCategory {
+                    case .fruits:
+                        List {
+                            ForEach(Fruit.allCases, id: \.rawValue) { fruit in
+                                Button(fruit.rawValue.capitalized) {
+                                    selectedFruit = fruit
+                                }
+                            }
+                        }
+                    case .vegetables:
+                        EmptyView()
+                    case .meats:
+                        EmptyView()
+                    }
+                }
+                .navigationTitle(selectedCategory.rawValue.capitalized)
+            } else {
+                Text("Select a category to begin")
+            }
+        } detail: {
+            if let selectedFruit {
+                Text("You Selected: \(selectedFruit.rawValue)")
+                    .font(.largeTitle)
+                    .navigationTitle(selectedFruit.rawValue.capitalized)
+            } else {
+                Text("Select Something")
+            }
+        }
+    }
+}
+
+enum FoodCategory: String, CaseIterable {
+    case fruits, vegetables, meats
+}
+
+enum Fruit: String, CaseIterable {
+    case apple, mango, banana
+}
+
+#Preview {
+    NavigationSplitView2()
+}
