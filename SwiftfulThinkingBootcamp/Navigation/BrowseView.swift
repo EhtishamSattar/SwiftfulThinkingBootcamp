b5385399e902d31ef7d04991e7c1dce87bb93cb2
EhtishamSattar <ehtishamsattar888@gmail.com>
2024-08-20 20:26:30 +0500

    Navigating Two Steps back in NavigationStack

--- SwiftfulThinkingBootcamp/Navigation/BrowseView.swift
+++ SwiftfulThinkingBootcamp/Navigation/BrowseView.swift
@@ -6,43 +6,28 @@
 //
 
 import SwiftUI
+import Combine
 
 struct BrowseView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State var close : Bool = false
     
     var body: some View {
+        
+        
         NavigationStack {
             VStack(spacing: 20) {  // Using VStack to arrange the links vertically
-                NavigationLink(destination: FirstDestinationView()) {
-                    Text("Go to First View")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
+                NavigationLink(destination: FirstDestinationView(close : $close)) {
+                                    Text("Go to First View")
+                                        .padding()
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(8)
+                                }
                 
-                NavigationLink(destination: SecondDestinationView()) {
-                    Text("Go to Second View")
-                        .padding()
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
             }
             .navigationTitle("Browse")
             .navigationBarBackButtonHidden(true)
-            .toolbar{
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.blue)
-                            .bold()
-                            .font(.title)
-                    }
-                }
-            }
         }
     }
 }
@@ -50,15 +35,25 @@ struct BrowseView: View {
 
 struct FirstDestinationView: View {
     @Environment(\.presentationMode) var presentationMode
-    
+    @Binding var close : Bool
     var body: some View {
         VStack {
-            Text("First Destination View")
-                .font(.largeTitle)
-                .bold()
-                .multilineTextAlignment(.center)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            NavigationLink(destination: SecondDestinationView(close : $close)) {
+                Text("Go to Second View")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
         }
+        .onChange(of: close) { newValue in
+            print(close)
+                   if newValue {
+                       presentationMode.wrappedValue.dismiss()
+                       // Optionally reset the state if needed
+                       close = false
+                   }
+               }
         .navigationTitle("First View")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
@@ -79,15 +74,26 @@ struct FirstDestinationView: View {
 
 struct SecondDestinationView: View {
     @Environment(\.presentationMode) var presentationMode
+    @Binding var close : Bool
     
     var body: some View {
         VStack {
-            Text("Second Destination View")
-                .font(.largeTitle)
-                .bold()
-                .multilineTextAlignment(.center)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            NavigationLink(destination: ThirdDestinationView(close : $close)) {
+                Text("Go to Third View")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
         }
+        .onChange(of: close) { newValue in
+            print(close)
+                   if newValue {
+                       presentationMode.wrappedValue.dismiss()
+                       // Optionally reset the state if needed
+                       close = false
+                   }
+               }
         .navigationTitle("Second View")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
@@ -103,6 +109,44 @@ struct SecondDestinationView: View {
                 }
             }
         }
+    }
+}
+
+
+struct ThirdDestinationView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var close : Bool
+    
+    let passthrough = PassthroughSubject<Bool, Never>()
+    var body: some View {
+        VStack {
+            Text("Third Destination View")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .navigationTitle("Third View")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                    close.toggle()
+                    //sendPassThrough(flag: true)
+                }) {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.blue)
+                        .bold()
+                        .font(.title)
+                }
+            }
+        }
+    }
+    
+    func sendPassThrough(flag : Bool){
+        passthrough.send((flag))
     }
 }
 
